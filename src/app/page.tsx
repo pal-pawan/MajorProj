0439aa6e08646155ed345464cf71447ad20e382e
Pawan Pal <pawanpal2001@gmail.com>
2025-04-23 11:32:07 +0530

    update: pending changes to the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 // import PreparationWindow from '@/components/PreparationWindow';
 import "regenerator-runtime/runtime";
 import WelcomeWindow from '@/components/WelcomeWindow';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 
 const page = () => {
   return (
     <>
-      <main className='bg-[#181818] h-screen w-full flex justify-center items-center text-white'>
+      <main className='bg-[#181818] h-screen w-full flex flex-col justify-center items-center text-white'>
         {/* <div>Landing Page</div> */}
         {/* <PreparationWindow/> */}
         <WelcomeWindow/>
+        <div className='flex gap-5 '>
+        <Button className='bg-white text-black font-bold text-lg' variant={'outline'}><Link href={'../sign-in'}>Sign In</Link></Button>
+        <Button className='bg-white text-black font-bold text-lg' variant={'outline'}><Link href={'../sign-up'}>Sign Up</Link></Button>
+        </div>
       </main>
     </>
   )
